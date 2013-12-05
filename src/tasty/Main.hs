75bdee606e4375873110e500af1f57b563af6d46
Alexandre Boily <alexandre.boily@savoirfairelinux.com>
2013-12-05 16:33:13 -0500

    Add tasty from 2013-12-03
    Slightly tweaked from the
    [original](https://github.com/ocharles/blog/blob/master/code/2013-12-03-tasty-example.hs),
    in order to suppress GHC's warnings.

---
+++ src/tasty/Main.hs
@@ -0,0 +1,28 @@
+import Test.Tasty
+import Test.Tasty.SmallCheck
+import Test.Tasty.HUnit
+
+import Data.List
+
+
+
+main :: IO ()
+main = defaultMain $
+  testGroup "Tests"
+    [ testGroup "(checked by SmallCheck)"
+        [ testProperty "sort == sort . reverse" $
+            \list -> sort (list :: [Int]) == sort (reverse list)
+
+        , testProperty "Fermat's last theorem" $
+            \x y z n -> (n :: Integer) >= 3 ==>
+              x^n + y^n /= (z^n :: Integer)
+        ]
+
+    , testGroup "Unit tests"
+        [ testCase "List comparison (different length)" $
+            ([1, 2, 3] :: [Integer]) `compare` [1,2] @?= GT
+
+        , testCase "List comparison (same length)" $
+            ([1, 2, 3] :: [Integer]) `compare` [1,2,2] @?= LT
+        ]
+    ]
