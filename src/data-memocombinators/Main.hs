04528265fd2922d8bab4f43ee563d71c19ed8814
Alexandre Boily <alexandre.boily@savoirfairelinux.com>
2013-12-09 13:38:22 -0500

    Data-memocombinators

---
+++ src/data-memocombinators/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import qualified Data.MemoCombinators as Memo
+
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fibFast :: Int -> Int
+fibFast = Memo.integral fib'
+ where fib' 0 = 1
+       fib' 1 = 1
+       fib' n = fibFast (n - 1) + fibFast (n - 2)
+
+main :: IO ()
+main = print $ fibFast 100
